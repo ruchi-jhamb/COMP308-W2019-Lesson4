7b74b0cebd0fcea44682d4ee6f2ea2bdb711d805
Ruchi <r144@my.centennialcollege.ca>
2019-03-27 04:26:28 -0400

    added uri

--- config/db.js
+++ config/db.js
@@ -2,5 +2,6 @@ module.exports = {
   /*
   URI: "mongodb://localhost/test"
   */
-  URI: "mongodb + srv://ruchi:<12345>@cluster0-vyciz.azure.mongodb.net/test"
+  URI:
+    "mongodb + srv://ruchi:<12345>@cluster0-vyciz.azure.mongodb.net/comp308-w2019-lesson4"
 };
