aba664fd3b406d6ac6b7a1c814fefc52f5f8a7f1
Ruchi <r144@my.centennialcollege.ca>
2019-03-27 05:11:24 -0400

    changed uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 module.exports = {
-  /*
-  URI: "mongodb://localhost/test"
-  */
+  //  URI: "mongodb://localhost/test"
+
   URI:
-    "mongodb + srv://ruchi:12345@cluster0-vyciz.azure.mongodb.net/COMP308-W2019-Lesson4"
+    "mongodb+srv://ruchi:12345@cluster-vyciz.azure.mongodb.net/COMP308-W2019-Lesson4"
 };
